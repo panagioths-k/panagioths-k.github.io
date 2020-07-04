b54d45f06b704ad38f43501f629fb7da2b484ddb
panagioths-k <panos_kotsikoris@hotmail.com>
2020-07-04 18:48:51 +0300

    test time

--- function.js
+++ function.js
@@ -12,29 +12,17 @@ var notify = function (title, message) {
   
 	var dtNow = new Date();
   
-	var dtNow1 = new Date();	dtNow1.setSeconds(dtNow1.getSeconds() + 5);		var dts1 = Math.floor(dtNow1);
-	var dtNow2 = new Date();	dtNow2.setSeconds(dtNow2.getSeconds() + 10);	var dts2 = Math.floor(dtNow2);
-	var dtNow3 = new Date();	dtNow3.setSeconds(dtNow3.getSeconds() + 15);	var dts3 = Math.floor(dtNow3);
-	var dtNow4 = new Date();	dtNow4.setSeconds(dtNow4.getSeconds() + 20);	var dts4 = Math.floor(dtNow4);
+	var dtNow1 = new Date();	dtNow1.setSeconds(dtNow1.getSeconds() + 10);		var dts1 = Math.floor(dtNow1);
+	var dtNow2 = new Date();	dtNow2.setMinutes(dtNow2.getMinutes() + 1);			var dts2 = Math.floor(dtNow2);
   
   var notification1 = new Notification(title, {
-      body: "BODY",
+      body: "BODY 1",
       timestamp: dts1
   });
   
   var notification2 = new Notification(title, {
-      body: "BODY",
+      body: "BODY 2",
       timestamp: dts2
-  });
-  
-  var notification3 = new Notification(title, {
-      body: "BODY",
-      timestamp: dts3
-  });
-  
-  var notification4 = new Notification(title, {
-      body: "BODY",
-      timestamp: dts4
   });
 };
 
